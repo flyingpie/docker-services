8107b6fffcde5fbaf4fc0e0dc4c67a3ef39d9cc9
Marco van den Oever <marco@flyingpie.nl>
2015-04-01 09:16:21 +0200

    Updated start-proxy.sh

--- start-proxy.sh
+++ start-proxy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 docker rm -f proxy
-docker run --name proxy -d -p 80:80 -p 443:443 \
--v /var/docker/proxy/config:/etc/nginx/conf.d \
--v /var/docker/proxy/certs:/etc/nginx/certs \
--v /var/run/docker.sock:/tmp/docker.sock \
-jwilder/nginx-proxy
+docker run --name proxy -d \
+  -p 80:80 \
+  -p 443:443 \
+  -v /var/docker/proxy/certs:/etc/nginx/certs \
+  -v /var/docker/proxy/my_proxy.conf:/etc/nginx/conf.d/my_proxy.conf:ro \
+  -v /var/run/docker.sock:/tmp/docker.sock \
+  jwilder/nginx-proxy
