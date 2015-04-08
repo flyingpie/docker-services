1d2b3c021e44d8c83a363e08dfb3b0e9703a929c
Marco van den Oever <marco@flyingpie.nl>
2015-04-08 19:12:00 +0200

    Added COPS

--- startup/start-cops.sh
+++ startup/start-cops.sh
@@ -4,6 +4,8 @@ source variables.sh
 
 docker rm -f cops
 
-docker run --name cops -it \
+docker run --name cops -d \
+  -e VIRTUAL_HOST=$VHOST_BOOKS \
   -p 80 \
+  -v /var/docker/owncloud/data/marco/files/clientsync/Books/:/metadata/ \
   flyingpie/cops
--- startup/start-proxy.sh
+++ startup/start-proxy.sh
@@ -9,6 +9,7 @@ docker run --name proxy -d \
   -p 80:80 \
   -p 443:443 \
   -v /var/docker/proxy/certs:/etc/nginx/certs \
+  -v /var/docker/proxy/htpasswd:/etc/nginx/htpasswd \
   -v /var/docker/proxy/proxy.conf:/etc/nginx/conf.d/proxy.conf:ro \
   -v /var/run/docker.sock:/tmp/docker.sock \
   -v /etc/localtime:/etc/localtime:ro \
