eca09eb12f6206deaa0a06c2dbf7f9d2a20d7522
Marco van den Oever <marco@flyingpie.nl>
2015-04-11 00:40:17 +0200

    Added spotweb startup

---
+++ startup/start-spotweb.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source variables.sh
+
+docker rm -f spotweb
+
+docker run --name spotweb -d \
+  --link mysql:mysql \
+  -e VIRTUAL_HOST=$VHOST_SPOTWEB \
+  -p 80 \
+  -v /var/docker/spotweb/config/dbsettings.inc.php:/var/www/site/spotweb/dbsettings.inc.php \
+  andyverbunt/docker-spotweb
