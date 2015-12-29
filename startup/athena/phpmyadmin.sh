3020e6b03ccf9d4b0013d913d4e309344284edaf
Marco van den Oever <marco@flyingpie.nl>
2015-12-29 01:47:28 +0100

    Updated phpmyadmin Updated camerasync

--- startup/athena/phpmyadmin.sh
+++ startup/athena/phpmyadmin.sh
@@ -2,12 +2,13 @@
 
 source variables.sh
 
+docker pull phpmyadmin/phpmyadmin
+
 docker rm -f phpmyadmin
 
 docker run --name phpmyadmin -d \
   --link mysql:mysql \
-  -e MYSQL_USERNAME=$MYSQL_ROOT_USER \
-  -e MYSQL_PASSWORD=$MYSQL_ROOT_PASSWORD \
+  -e PMA_HOST=mysql \
   -e VIRTUAL_HOST=phpmyadmin.flyingpie.nl \
   --restart=always \
-  corbinu/docker-phpmyadmin
+  phpmyadmin/phpmyadmin
