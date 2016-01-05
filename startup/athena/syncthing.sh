97aef926ab32c0a71fa8c3d52d607c4c8ff2bfde
Marco van den Oever <marco@flyingpie.nl>
2016-01-05 19:43:36 +0100

    Updated syncthing

--- startup/athena/syncthing.sh
+++ startup/athena/syncthing.sh
@@ -2,14 +2,14 @@
 
 docker rm -f syncthing
 
-docker pull istepanov/syncthing
+docker pull tianon/syncthing
 
 docker run --name syncthing -d \
   -p 8008:8384 \
   -p 22000:22000 \
   -p 21025:21025/udp \
-  -v /var/backup/syncthing/config:/home/syncthing/.config/syncthing \
-  -v /var/docker:/home/syncthing/docker-athena \
-  -v /var/backup/docker-apollo:/home/syncthing/docker-apollo \
-  --restart=always \
-  istepanov/syncthing
+  -v /var/backup/syncthing/config:/home/user/.config/syncthing \
+  -v /var/docker:/home/user/docker-athena:ro \
+  -v /var/backup/docker-apollo:/home/user/docker-apollo \
+  --restart always \
+  tianon/syncthing
